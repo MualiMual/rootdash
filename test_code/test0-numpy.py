778574423852e3da9c41ca85696f645746f9d1e4
X JaMual <mualimualvisuals@gmail.com>
2025-01-22 21:22:15 -0500

    Delete test_code directory

--- test_code/test0-numpy.py
+++
@@ -1,3 +0,0 @@
-import numpy as np
-print(np.__version__)
-print(np.array([1, 2, 3]))
--- test_code/tpuServer.py
+++
@@ -1,7 +0,0 @@
-from bottle import route, run
-
-@route('/')
-def index():
-    return "<h1>Welcome to the TPU Server</h1><p>Object detection is running.</p>"
-
-run(host='0.0.0.0', port=8081, debug=True, reloader=True)
